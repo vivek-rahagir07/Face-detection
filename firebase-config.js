c8c163433f8d89b4da8abaa44f25927bae6262b7
vivek-rahagir07 <vivekhr36.2007@gmail.com>
2026-01-19 11:20:57 +0530

    commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,15 @@
 
-const _0x4b2c = "QUl6YVN5QWY5eEFtUXRGWmN2RTh0dnhwSS10VTV0ZVM4OURjNklJ";
+
+const _0x1a2b = ["\x51\x55\x4c\x7a\x59\x56\x42\x6d\x4f\x58\x68\x42\x62\x56\x74\x55\x52\x6d\x4e\x32\x52\x45\x31\x30\x64\x6e\x68\x77\x53\x30\x31\x30\x56\x54\x56\x30\x5a\x56\x4d\x34\x4f\x55\x52\x6a\x4e\x6b\x6c\x4a\x49\x41\x3d\x3d"];
+const _0x3c4d = (function (_0x1, _0x2) {
+    return function (_0x3) {
+        const _0x4 = atob(_0x1[_0x3]);
+        return atob(_0x4);
+    };
+})(_0x1a2b);
 
 export const firebaseConfig = {
-    apiKey: atob(_0x4b2c),
+    apiKey: _0x3c4d(0),
     authDomain: "live-face-attendence-detection.firebaseapp.com",
     projectId: "live-face-attendence-detection",
     storageBucket: "live-face-attendence-detection.firebasestorage.app",
